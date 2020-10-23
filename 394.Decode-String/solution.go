4657ef9ea6509aca276b9af3acc89c8ae60e135e
SpiffyEight77 <killer_0248@live.com>
2020-10-23 21:37:05 +0800

    docs: add new solution and change the dir

---
+++ 394.Decode-String/solution.go
@@ -0,0 +1,37 @@
+package leetcode
+
+import "strconv"
+
+func decodeString(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case ']':
+			temp := make([]byte, 0)
+			for len(stack) != 0 && stack[len(stack)-1] != '[' {
+				v := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				temp = append(temp, v)
+			}
+			stack = stack[:len(stack)-1]
+			idx := 1
+			for len(stack) >= idx && stack[len(stack)-idx] >= '0' && stack[len(stack)-idx] <= '9' {
+				idx++
+			}
+			num := stack[len(stack)-idx+1:]
+			stack = stack[:len(stack)-idx+1]
+			count, _ := strconv.Atoi(string(num))
+			for j := 0; j < count; j++ {
+				for j := len(temp) - 1; j >= 0; j-- {
+					stack = append(stack, temp[j])
+				}
+			}
+		default:
+			stack = append(stack, s[i])
+		}
+	}
+	return string(stack)
+}
