c875b708720fefb933308d694eabc4a6e1408a3c
SpiffyEight77 <killer_0248@live.com>
2020-10-31 23:43:29 +0800

    docs: add & modify solutions

--- linked-list/206.Reverse-Linked-List/solution.go
+++ linked-list/206.Reverse-Linked-List/solution.go
@@ -6,13 +6,26 @@ type ListNode struct {
 }
 
 func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
 	var prev *ListNode
+
 	for head != nil {
 		temp := head.Next
 		head.Next = prev
-
 		prev = head
 		head = temp
 	}
 	return prev
+}
+
+func reverseListV2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	cur := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return cur
 }
--- linked-list/24.Swap-Nodes-in-Pairs/solution.go
+++ linked-list/24.Swap-Nodes-in-Pairs/solution.go
@@ -9,16 +9,16 @@ func swapPairs(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
-	dummy := &ListNode{Val: 0}
-	dummy.Next = head
-	prev := dummy
-	for prev.Next != nil && prev.Next.Next != nil {
-		node1 := prev.Next
-		node2 := prev.Next.Next
-		prev.Next = node2
+	dummy := &ListNode{0, head}
+	head = dummy
+
+	for head.Next != nil && head.Next.Next != nil {
+		node1 := head.Next
+		node2 := head.Next.Next
+		head.Next = node2
 		node1.Next = node2.Next
 		node2.Next = node1
-		prev = node1
+		head = head.Next.Next
 	}
 	return dummy.Next
 }
