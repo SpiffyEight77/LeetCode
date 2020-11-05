9d1753cd901fd726b3cbff546e4e59d3a628ca5f
SpiffyEight77 <killer_0248@live.com>
2020-11-05 22:30:51 +0800

    docs: add new solutions

--- 941.Valid-Mountain-Array/solution.go
+++ 941.Valid-Mountain-Array/solution.go
@@ -4,4 +4,16 @@ func validMountainArray(A []int) bool {
 	if len(A) < 3 {
 		return false
 	}
+
+	start, end := 0, len(A)-1
+	for start < end && A[start] < A[start+1] {
+		start++
+	}
+	for start < end && A[end] < A[end-1] {
+		end--
+	}
+	if start == 0 || end == len(A)-1 {
+		return false
+	}
+	return true
 }
--- Sort/75.Sort-Colors/solution.go
+++ Sort/75.Sort-Colors/solution.go
@@ -1,31 +1,13 @@
 package leetcode
 
+import "sort"
+
 func sortColors(nums []int) {
 	if len(nums) == 0 {
 		return
 	}
-	quickSort(nums, 0, len(nums))
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
 	return
-}
-
-func quickSort(nums []int, start, end int) {
-	if start < end {
-		pivot := partition(nums, start, end)
-		quickSort(nums, 0, pivot-1)
-		quickSort(nums, pivot+1, end)
-	}
-}
-
-func partition(nums []int, start, end int) int {
-	p := nums[end]
-	i := start
-
-	for j := start; j < end; j++ {
-		if nums[j] < p {
-			nums[j], nums[i] = nums[i], nums[j]
-			i++
-		}
-	}
-	nums[end], nums[i] = nums[i], nums[end]
-	return i
 }
