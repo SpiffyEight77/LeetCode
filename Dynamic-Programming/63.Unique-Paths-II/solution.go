4e13b886bd76745408644a4774601135db361440
SpiffyEight77 <killer_0248@live.com>
2020-11-13 23:53:57 +0800

    docs: add new soluton

---
+++ Dynamic-Programming/63.Unique-Paths-II/solution.go
@@ -0,0 +1,43 @@
+package leetcode
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	if obstacleGrid[0][0] == 1 {
+		return 0
+	}
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+	f := make([][]int, m)
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if f[i] == nil {
+				f[i] = make([]int, n)
+			}
+			f[i][j] = 1
+		}
+	}
+
+	for i := 1; i < m; i++ {
+		if obstacleGrid[i][0] == 1 || f[i-1][0] == 0 {
+			f[i][0] = 0
+		}
+	}
+
+	for j := 1; j < n; j++ {
+		if obstacleGrid[0][j] == 1 || f[0][j-1] == 0 {
+			f[0][j] = 0
+		}
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				f[i][j] = 0
+			} else {
+				f[i][j] = f[i][j-1] + f[i-1][j]
+			}
+		}
+	}
+
+	return f[m-1][n-1]
+}
