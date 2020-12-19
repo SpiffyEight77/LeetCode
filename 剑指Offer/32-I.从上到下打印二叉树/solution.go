9d07fae12a94c799b392f8380bca2f25db9b6a4d
SpiffyEight77 <killer_0248@live.com>
2020-12-19 21:05:26 +0800

    docs: add new solution

---
+++ 剑指Offer/32-I.从上到下打印二叉树/solution.go
@@ -0,0 +1,32 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	res := make([]int, 0)
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			res = append(res, node.Val)
+		}
+	}
+	return res
+}
